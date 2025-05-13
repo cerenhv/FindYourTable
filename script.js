6137c246c01d5bf2437ab7d092e170610ea4ad49
Ceren Havaylar <41002336+cerenhv@users.noreply.github.com>
2025-05-13 15:08:58 +0300

    Added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const guestList = {
+      "Ceren Havaylar": "Masa 1",
+      "Erdoğan Havaylar": "Masa 1"
+      "Hediye Bilgili": "Masa 3",
+      "Ebru Gönen": "Masa 6"
+};
+
+function findTable() {
+  const nameInput = document.getElementById("guestName").value.toLowerCase().trim();
+  const result = document.getElementById("result");
+  
+  if (guestList[nameInput]) {
+    result.textContent = `${nameInput.toUpperCase()} ${guestList[nameInput]} numaralı masada oturuyor.`;
+  } else {
+    result.textContent = "Bu isim listede bulunamadı.";
+  }
+}
