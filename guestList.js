09ad78f1da468812e7e9772107200dc593660e99
Ceren Havaylar <41002336+cerenhv@users.noreply.github.com>
2025-05-14 13:37:14 +0300

    Create guestList.js

---
+++ guestList.js
@@ -0,0 +1,7 @@
+const guestList = {
+      "Ceren Havaylar": "Yuvarlak - 1",
+      "Erdoğan Havaylar": "Yuvarlak - 1",
+      "Hediye Bilgili": "Yuvarlak - 3",
+      "Ebru Gönen": "Yuvarlak - 6",
+      "Ceren Test": "Dikdörtgen - 4"
+    };
